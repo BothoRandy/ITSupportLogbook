441bea48aee203e2bb008d500ae97eba850c2387
Boago <b.senopiwa@gmail.com>
2026-05-08 11:41:37 +0200

    created a DTO folder under models for request and response models

---
+++ ITSupportLogbook/Models/DTOs/RegisterUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ITSupportLogbook.Models.DTOs
+{
+    public class RegisterUserDto
+    {
+        public required string FirstName { get; set; } = string.Empty;
+        public required string LastName { get; set; } = string.Empty;
+        public required string Password { get; set; } = string.Empty;
+    }
+}
